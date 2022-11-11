6bae56fdc003f30d75a4d4f0c4243213b0d9d741
dannyxyxy <116336429+dannyxyxy@users.noreply.github.com>
2022-11-11 17:16:38 +0900

    Add files via upload

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 mapOption = { 
-    center: new kakao.maps.LatLng(37.495711,127.0717957), // 지도의 중심좌표
+    center: new kakao.maps.LatLng(36.9910113,127.9259497), // 지도의 중심좌표
     level: 12 // 지도의 확대 레벨
 };  
 
@@ -67,6 +67,28 @@ marker.setMap(map);
 
 var iwContent = '<div style="padding:5px;">2022 보드게임페스타 <br><a href="https://map.kakao.com/link/map/2022보드게임페스타,37.495711,127.0717957" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/2022보드게임페스타,37.495711,127.0717957" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
     iwPosition = new kakao.maps.LatLng(37.495711, 127.0717957); //인포윈도우 표시 위치입니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+    position : iwPosition, 
+    content : iwContent 
+});
+  
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+infowindow.open(map, marker);
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(34.9548461, 127.4824741); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+var iwContent = '<div style="padding:5px;">문화인 쌀롱데이 <br><a href="https://map.kakao.com/link/map/문화인쌀롱데이,34.9548461,127.4824741" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/문화인쌀롱데이,34.9548461,127.4824741" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwPosition = new kakao.maps.LatLng(34.9548461, 127.4824741); //인포윈도우 표시 위치입니다
 
 // 인포윈도우를 생성합니다
 var infowindow = new kakao.maps.InfoWindow({
