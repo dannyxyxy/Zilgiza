4a9330fb63a59953bd9bd9387db5839675272a79
soeyeon <57591478+soeyeon@users.noreply.github.com>
2022-11-16 12:01:29 +0900

    Add files via upload

--- js/NewEvent.js
+++ js/NewEvent.js
@@ -3,6 +3,7 @@ function submit_event()
     var result = confirm("작성한 글을 게시하시겠습니까?");
     if(result){
         alert("게시하였습니다.");
+        location.href="./Community.html";
     }else{
         alert("취소하였습니다.");
     }
